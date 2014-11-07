ca6a0673ba4f1c54d727ed51996961df4474b5b0
Bill <liangwnag18@creditease.cn>
2014-11-08 00:25:31 +0800

    test

---
+++ GitIgnoreTest/Test1.h
@@ -0,0 +1,13 @@
+//
+//  Test1.h
+//  GitIgnoreTest
+//
+//  Created by Bill on 14-11-8.
+//  Copyright (c) 2014年 V+. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test1 : NSObject
+
+@end
---
+++ GitIgnoreTest/Test1.m
@@ -0,0 +1,13 @@
+//
+//  Test1.m
+//  GitIgnoreTest
+//
+//  Created by Bill on 14-11-8.
+//  Copyright (c) 2014年 V+. All rights reserved.
+//
+
+#import "Test1.h"
+
+@implementation Test1
+
+@end
